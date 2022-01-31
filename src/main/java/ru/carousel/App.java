a70f3d6bef602913fb31b68cfe6243bc6f40bb96
kkarapull <90611738+kkarapull@users.noreply.github.com>
2022-01-31 02:15:13 -0500

    Add files via upload

---
+++ src/main/java/ru/carousel/App.java
@@ -0,0 +1,9 @@
+package ru.carousel;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(new AppBean("Maven").sayHello());
+    }
+}
---
+++ src/test/java/ru/carousel/AppTest.java
@@ -0,0 +1,44 @@
+package ru.carousel;
+
+import org.junit.Assert;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    /**
+     * Create the test case
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    public void testAssert()
+    {
+		 String message = new AppBean("Maven 2").sayHello();
+         Assert.assertEquals("Test Hello Machine", "Hello, Maven 2", message);
+    }
+}
